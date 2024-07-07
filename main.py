defc61617a5f0d25036f1442f91c778086a7f2fd
Spandan Suthar <spandan@Spandans-MacBook-Pro.local>
2024-07-07 17:05:14 +0530

    format

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ import os
 import numpy as np
 import torch
 
-torch.set_default_tensor_type(torch.cuda.FloatTensor
-                                    if torch.cuda.is_available()
-                                    else torch.FloatTensor)
+torch.set_default_tensor_type(
+    torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+)
 # torch.set_default_tensor_type(torch.FloatTensor)
 try:
-    os.mkdir('results/')
+    os.mkdir("results/")
 except FileExistsError:
     pass
 
@@ -34,13 +34,21 @@ n_layers = 8
 dropout = 0
 minibatch = 10000
 
-model = TransformerModel(system_sizes, param_dim, embedding_size, n_head, n_hid, n_layers, dropout=dropout,
-                         minibatch=minibatch)
+model = TransformerModel(
+    system_sizes,
+    param_dim,
+    embedding_size,
+    n_head,
+    n_hid,
+    n_layers,
+    dropout=dropout,
+    minibatch=minibatch,
+)
 num_params = sum([param.numel() for param in model.parameters()])
-print('Number of parameters: ', num_params)
-folder = 'results/'
+print("Number of parameters: ", num_params)
+folder = "results/"
 name = type(Hamiltonians[0]).__name__
-save_str = f'{name}_{embedding_size}_{n_head}_{n_layers}'
+save_str = f"{name}_{embedding_size}_{n_head}_{n_layers}"
 # missing_keys, unexpected_keys = model.load_state_dict(torch.load(f'{folder}ckpt_100000_{save_str}_0.ckpt'),
 #                                                       strict=False)
 # print(f'Missing keys: {missing_keys}')
@@ -53,5 +61,12 @@ point_of_interest = None
 use_SR = False
 
 optim = Optimizer(model, Hamiltonians, point_of_interest=point_of_interest)
-optim.train(100000, batch=1000000, max_unique=100, param_range=param_range,
-            fine_tuning=False, use_SR=use_SR, ensemble_id=int(use_SR))
+optim.train(
+    100000,
+    batch=1000000,
+    max_unique=100,
+    param_range=param_range,
+    fine_tuning=False,
+    use_SR=use_SR,
+    ensemble_id=int(use_SR),
+)
