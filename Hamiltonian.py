3b179c256671a60796410ce35705f31b7ce3ec31
spagdoon0411 <spandan.suthar@live.com>
2024-07-20 11:59:31 -0700

    add generate_basis to Ising

--- Hamiltonian.py
+++ Hamiltonian.py
@@ -247,6 +247,12 @@ class Ising(Hamiltonian):
             self.Hamiltonian = self.Hamiltonian + h * hX
         return self.Hamiltonian
 
+    def generate_basis(self):
+        basis = np.zeros((2**self.n, self.n), dtype=int)
+        for i in range(2**self.n):
+            basis[i] = np.array([int(b) for b in np.binary_repr(i, width=self.n)])
+        return basis
+
     def DMRG(self, param=None, verbose=False, conserve=None):
         # Tenpy has S_i = 0.5 sigma_i, mine doesn't have the 0.5
         # some constants are added to fix the 0.5
