b0682b770e55c3d6d9c15cb1ab8b104a6991c3cb
spagdoon0411 <spandan.suthar@live.com>
2024-08-02 10:32:25 -0700

    add brief description of angular loss

--- model/loss_functions.py
+++ model/loss_functions.py
@@ -1,6 +1,11 @@
 import torch
 
 def angular_loss_sq(angle, target):
+    """
+    A loss function based on the distance between two angles with the 
+    theta = theta + 2 * pi invariance, target-predicted exchange invariance,
+    and common phase shift invariance.
+    """
     angle = torch.remainder(angle, 2 * torch.pi)
     target = torch.remainder(target, 2 * torch.pi)
     diff = torch.abs(angle - target)
