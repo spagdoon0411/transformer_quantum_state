2cebfe7b2af981ac318f2ca199555ed137811ec5
spagdoon0411 <spandan.suthar@live.com>
2024-08-08 11:36:21 -0700

    set learning rate in Optimizer, not Adam

--- training/supervised_script.py
+++ training/supervised_script.py
@@ -1,9 +1,9 @@
 import torch
 import numpy as np
 import os
-import json
 from hamiltonians.Ising import Ising
-from model.model import TransformerModel
+
+from model.model_batched import TransformerModel
 from optimizers.optimizer_supervised_batches import Optimizer
 from torch.utils.tensorboard import SummaryWriter
 import pickle
@@ -24,18 +24,20 @@ gpu_setup()
 torch.set_default_device("cuda")
 torch.set_default_dtype(torch.float32)
 
-system_sizes = torch.arange(10, 15 + 1, 2).reshape(-1, 1)
+system_sizes = torch.arange(15, 15 + 1, 1).reshape(-1, 1)
 Hamiltonians = [Ising(size, periodic=True, get_basis=True) for size in system_sizes]
-# data_dir_path = os.path.join("TFIM_ground_states", "2024-08-02T12-12-55.238")
-data_dir_path = os.path.join("TFIM_ground_states", "h_0.6")
+data_dir_path = os.path.join("TFIM_ground_states", "h_0.6_new")
 
-perc = (2**15 - 30000) / 2**15
-batch_size_dyn = lambda n: int(2**n * (1 - perc))
+def batch_from_n(n: int):
+    # Reference point: batch_size_dyn(15) = 16384
+    return int(2 ^ (30 - n - 1))
+
 
 for ham in Hamiltonians:
     ham.load_dataset(
         data_dir_path,
-        batch_size=batch_size_dyn(ham.n),
+        # batch_size=batch_from_n(ham.n),
+        batch_size=30000,
     )
 
 param_dim = Hamiltonians[0].param_dim
@@ -49,24 +51,32 @@ param_range = None
 point_of_interest = None
 use_SR = False
 
-testmodel = TransformerModel(
-    system_sizes,
-    param_dim,
-    embedding_size,
-    n_head,
-    n_hid,
-    n_layers,
-    dropout=dropout,
+compat_dict = {
+    "system_sizes": system_sizes,
+    "param_range": None,
+}
+
+model = TransformerModel(
+    n_dim=1,
+    param_dim=param_dim,
+    embedding_size=embedding_size,
+    n_head=n_head,
+    n_hid=n_hid,
+    n_layers=n_layers,
+    possible_spin_vals=2,
+    compat_dict=compat_dict,
+    dropout_encoding=dropout,
+    dropout_transformer=dropout,
     minibatch=minibatch,
 )
 
-testmodel.cuda()
+model.cuda()
 
 results_dir = "results"
 paper_checkpoint_name = "ckpt_100000_Ising_32_8_8_0.ckpt"
 paper_checkpoint_path = os.path.join(results_dir, paper_checkpoint_name)
 checkpoint = torch.load(paper_checkpoint_path)
-testmodel.load_state_dict(checkpoint)
+model.load_state_dict(checkpoint)
 
 drmg40path = os.path.join("results", "E_dmrg_40.npy")
 dmrg40 = np.load(drmg40path)
@@ -82,13 +92,9 @@ dmrg40_h_values = torch.linspace(0, 2, 101)
 oneidx = torch.where(dmrg40_h_values.isclose(torch.tensor(0.6)))[0][0]
 print(f"Investigating h = {0.6} at index {oneidx}")
 
-opt = Optimizer(testmodel, Hamiltonians, point_of_interest=point_of_interest)
+opt = Optimizer(model, Hamiltonians, lr=1e-7, beta1=0.9, beta2=0.98, point_of_interest=point_of_interest)
 
-opt.optim = torch.optim.Adam(
-    opt.model.parameters(), lr=0.5, betas=(0.9, 0.98), eps=1e-9
-)
-
-TRIAL_NUM = 53
+TRIAL_NUM = 70
 
 writer = SummaryWriter(f"runs/h=0.6_trial{TRIAL_NUM}")
 
