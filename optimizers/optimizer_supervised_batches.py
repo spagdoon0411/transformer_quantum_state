7149a23f7aba1ea33f794101f5f8be57987ea2ad
spagdoon0411 <spandan.suthar@live.com>
2024-08-08 11:25:08 -0700

    use relative paths and create dirs where needed

--- optimizers/optimizer_supervised_batches.py
+++ optimizers/optimizer_supervised_batches.py
@@ -23,6 +23,12 @@ from model.loss_functions import prob_phase_loss
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 from torch.utils.tensorboard import SummaryWriter
+import os
+from ray import train, tune
+from ray.train import Checkpoint, get_checkpoint
+from pathlib import Path
+import pickle
+import tempfile
 
 
 class Optimizer:
@@ -62,186 +68,6 @@ class Optimizer:
         self.save_freq = 100
         self.ckpt_freq = 100
         self.point_of_interest = point_of_interest
-
-    # A schedule used for an adaptive learning rate
-    @staticmethod
-    def lr_schedule(step, model_size, factor=5.0, warmup=4000, start_step=0):
-        # using the lr schedule from the paper: Attention is all you need
-        step = step + start_step
-        if step < 1:
-            step = 1
-        return factor * (
-            model_size ** (-0.5) * min(step ** (-0.75), step * warmup ** (-1.75))
-        )
-
-    def minimize_energy_step(self, H, batch, max_unique, use_symmetry=True):
-        """
-        Produces a computation graph for the energy expectation value of a Hamiltonian H.
-        Arguments:
-        - H: Hamiltonian object - Hamiltonian
-        - batch: number of samples to generate (see sample in model_utils.py) - int
-        - max_unique: maximum number of unique samples to generate (see sample) - int
-        - use_symmetry: whether to use symmetry in the model - bool
-        """
-        symmetry = H.symmetry if use_symmetry else None
-        samples, sample_weight = sample(self.model, batch, max_unique, symmetry)
-
-        # This is the expectation value of <H> over the probability distribution captured
-        # by the sample function (i.e., the probability distribution associated with the
-        # wave function represented by the model).
-        E = H.Eloc(samples, sample_weight, self.model, use_symmetry)
-        E_mean = (E * sample_weight).sum()
-        E_var = (
-            (((E - E_mean).abs() ** 2 * sample_weight).sum() / H.n**2)
-            .detach()
-            .cpu()
-            .numpy()
-        )
-        Er = (E_mean.real / H.n).detach().cpu().numpy()
-        Ei = (E_mean.imag / H.n).detach().cpu().numpy()
-
-        # NOTE: this is NOT the gradient of the loss function (the loss function being
-        # <H> over \psi* times \psi); compute_grad is a misnomer for this function.
-        # It should be called compute_loss (or, more verbosely, get_loss_computation_graph).
-        # The gradient of the <H> function is computed by calling .backward() on the first
-        # tuple member that compute_grad returns--and, in fact, .backward uses the
-        # those calculated derivatives to adjust the model's parameters in this step. (Abstracted
-        # away is reverse-mode automatic differentiation, which provides local gradients).
-
-        loss, log_amp, log_phase = compute_grad(
-            self.model, samples, sample_weight, E, symmetry
-        )
-        return loss, log_amp, log_phase, sample_weight, Er, Ei, E_var
-
-    def calculate_mse_step(self, H, params, basis_batch=None, use_symmetry=True):
-        """
-        Produces a computation graph for the mean squared error between the model's predictions and
-        the true ground state wave function for a Hamiltonian H.
-
-        Parameters:
-
-        H: Hamiltonian
-            The Hamiltonian object that the true wave function comes from. Either produces or
-            is (via a dataset passed as an argument) associated with a (J, psi(J)) pair.
-            TODO: consider whether datasets should be associated with Hamiltonians or passed into
-            optimizers.
-
-        params: torch.Tensor - (n_parameters, )
-            The parameters to set the model to, defining a point in parameter space (and thus a
-            Hamiltonian/wave function in the family that this model models)
-
-        basis_batch: int | None
-            The maximum number of sequences to pass into the model at once. If None, will
-            attempt to use an entire basis state dataset (for example, the entire N by 2^N
-            tensor for a 1/2-spin TFIM chain)
-
-        use_symmetry: bool
-            Whether to use symmetry in the compute_psi function (from model_utils.py) to
-            produce a wave function.
-
-        Returns:
-
-        loss: torch.Tensor - (1, ) TODO: can this be a scalar?
-            The mean squared error between the model's predictions and the true wave function.
-
-        """
-
-        # Construct a dataset of samples. This will be a tensor of shape
-        # (n, 2^n) where n is the number of spins, and will contain one of every
-        # basis state in the Hilbert space. The sample_weight tensor is not needed;
-        # compute_psi does not take one (and, considering how the authors describe how
-        # to obtain a wave function given a model output, it should not need one).
-        basis = H.basis
-
-        # TODO: How could we package these into batches across J? This might not involve
-        # fundamental changes to compute_psi, but it would involve changes to the forward pass.
-        # The forward pass assumes that one batch comes from a single point in sample
-        # space (using only a static J-vector).
-
-        # TODO: QUESTION: if the members of a batch were heterogenous
-        # with respect to J, would the model's forward pass still be correct?
-        # HYPOTHESIS: yes! After wrap_spins (in forward), the sequences are processed entirely
-        # in parallel. Sequences do not interact with each other--but members of the same sequence do
-        # (a fundamental guarantee of batch processing)
-
-        # compute_psi from model_utils.py is used here, producing two vectors:
-        # one for P(s, J) values and one for phi(s, J) values.
-        symmetry = H.symmetry if use_symmetry else None
-        log_amp, log_phase = compute_psi(
-            self.model, basis, symmetry, check_duplicate=True
-        )
-
-        amp = torch.exp(log_amp)
-        phase = torch.exp(1j * log_phase)
-
-        # Use (1) - (4) from the TQS paper--most significantly, (2)--to obtain
-        # the wave function that the model represents. NOTE: the discussion about
-        # symmetries in Appendix B is accounted for if a non-None symmetry was
-        # passed to compute_psi.
-        psi_predicted = amp.mul(torch.exp(1j * phase))
-
-        # Obtain the ground state wave function for the Hamiltonian H, possibly memoized internally
-        # in the Hamiltonian object. This is the true wave function that the model's predictions
-        # are compared against. TODO: implement memoization in Hamiltonian objects.
-        energy, psi_true = H.retrieve_ground(param=params)
-
-        # Compute the mean squared error between the model's predictions and the true
-        # ground state wave function.
-        psi_predicted_real_imag = torch.view_as_real(psi_predicted)
-        psi_true_real_imag = torch.view_as_real(psi_true.to(torch.complex64))
-
-        loss = F.mse_loss(
-            psi_predicted_real_imag, psi_true_real_imag
-        )  # TODO: better way to account for only a phase difference?
-
-        # At this point, we should have a computational graph for the mean squared error--
-        # i.e., a computational graph for a loss function--that we can backpropagate through
-        # using .backward() and .step(). This will adjust the model's parameters.
-        #
-        # TODO: how is the Attention Is All You Need learning rate involved? Is it a
-        # global PyTorch config setting?
-
-        return loss
-
-    def generate_parameter_range(self, start, end, step):
-        """
-        A simple generator returning the next value in a range of values
-        whenever called, according to a step size.
-        """
-        value = start
-        while value < end:
-            yield value
-            value += step
-
-    def generate_parameter_points(
-        self, parameter_ranges, step_sizes, distribution=None
-    ):
-        """
-        Generate all possible combinations of parameter values for a model
-        (i.e., the Cartesian product of values of parameters in a slice of parameter space)
-
-        Parameters:
-            parameter_ranges: torch.Tensor of shape (n_parameters, 2)
-                The starting and ending values for each dimension of the slice of parameter space
-            step_sizes: torch.Tensor of shape (n_parameters,)
-                The step size for each dimension of the slice of parameter space
-            distribution: N/A
-                TODO: Not implemented
-
-        """
-
-        if distribution is not None:
-            raise NotImplementedError(
-                "Sampling using a custom distribution is not implemented yet."
-            )
-
-        # Every possible individual parameter value for each parameter, in order
-        parameter_ranges = [
-            self.generate_parameter_range(start.item(), end.item(), step.item())
-            for (start, end), step in zip(parameter_ranges, step_sizes)
-        ]
-
-        return itertools.product(*parameter_ranges)
 
     def show_energy_report(
         self,
@@ -313,11 +139,6 @@ class Optimizer:
         Ei = (E_mean.imag / H.n).detach().cpu().numpy()
 
         return E_mean, E_var, Er, Ei
-
-    # Performs a mapping from one degenerate state to the other. Does not
-    # account for the parameter range.
-    def ising_degen(self, phases, probs):
-        return (-phases, probs)
 
     def plot_grad_flow(self, named_parameters, tensorboard_writer, step):
         ave_grads = []
@@ -413,10 +234,12 @@ class Optimizer:
         monitor_energies=None,
         param_range=None,
         ensemble_id=0,
-        start_iter=None,
         prob_weight=0.5,
         arg_weight=0.5,
         writer: SummaryWriter = None,
+        run_num = 0,
+        start_epoch = 0,
+        energy_error_frequency = 100
     ):
         """
         Trains the model to replicate the parameter-and-spin-sequence to ground
@@ -461,12 +284,12 @@ class Optimizer:
             f"{name}_{embedding_size}_{n_head}_{n_layers}_{ensemble_id}_supervised"
         )
 
-        scheduler = torch.optim.lr_scheduler.LambdaLR(
-            self.optim,
-            lambda step: self.lr_schedule(
-                step, self.model.embedding_size, start_step=start_iter
-            ),
-        )
+        # scheduler = torch.optim.lr_scheduler.LambdaLR(
+        #     self.optim,
+        #     lambda step: self.lr_schedule(
+        #         step, self.model.embedding_size, start_step=start_iter
+        #     ),
+        # )
 
         self.E_errors_all = []
 
@@ -474,11 +297,13 @@ class Optimizer:
             monitor_hamiltonians[0], monitor_params[0]
         )
 
-        initial_relative_error = torch.abs((initial_energy[0] - monitor_energies[0]) / monitor_energies[0])
+        initial_relative_error = torch.abs(
+            (initial_energy[0] - monitor_energies[0]) / monitor_energies[0]
+        )
 
         print(f"Initial energy error: {initial_relative_error}")
 
-        for i in range(epochs):
+        for i in range(start_epoch, epochs):
             epoch_start = time.time()
 
             E_errors = []
@@ -534,11 +359,6 @@ class Optimizer:
                     self.optim.zero_grad()
                     loss.backward()
                     if writer is not None:
-                    #     writer.add_scalar(
-                    #         f"N={system_size}_loss",
-                    #         loss.item(),
-                    #         i,
-                    #     )
                         self.plot_grad_flow(
                             self.model.named_parameters(),
                             writer,
@@ -553,14 +373,16 @@ class Optimizer:
                     )
 
                     if timestep_plotting_index % self.ckpt_freq == 0:
+                        if not os.path.isdir("supervised_results"):
+                            os.mkdir("supervised_results")
                         torch.save(
                             self.model.state_dict(),
-                            f"supervised_results/ckpt_{timestep_plotting_index}_{save_str}.ckpt",
+                            f"supervised_results/ckpt_run{run_num}_{timestep_plotting_index}_{save_str}.ckpt",
                         )
 
                     energy_start = time.time()
 
-                    if not monitor_params is None:
+                    if (not monitor_params is None) and (timestep_plotting_index % energy_error_frequency == 0):
                         self.show_energy_report(
                             monitor_params,
                             monitor_hamiltonians,
