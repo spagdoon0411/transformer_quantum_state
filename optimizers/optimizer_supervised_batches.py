29152bb2cb0e0fffae8152fbeb18dcb1ce079196
spagdoon0411 <spandan.suthar@live.com>
2024-08-09 07:54:38 -0700

    restore separation of concerns in optimizer_supervised_batches

--- optimizers/optimizer_supervised_batches.py
+++ optimizers/optimizer_supervised_batches.py
@@ -29,10 +29,22 @@ from ray.train import Checkpoint, get_checkpoint
 from pathlib import Path
 import pickle
 import tempfile
+from optimizers.bookkeeping_tools import (
+    average_monitor_dict,
+    create_save_dirs,
+)
 
 
 class Optimizer:
-    def __init__(self, model, Hamiltonians, lr=1e-7, beta1=0.9, beta2=0.98, point_of_interest=None):
+    def __init__(
+        self,
+        model,
+        Hamiltonians,
+        lr=1e-7,
+        beta1=0.9,
+        beta2=0.98,
+        point_of_interest=None,
+    ):
         # Transformer model to optimize
         self.model = model
 
@@ -68,41 +80,95 @@ class Optimizer:
         self.save_freq = 100
         self.ckpt_freq = 100
         self.point_of_interest = point_of_interest
+        self.energy_estimation_iter_frequency = 10
+        self.grad_flow_iter_frequency = 100
+        self.checkpointing_dir = None
+        self.monitoring_dir = None
+        self.tensorboard_dir = None
 
-    def show_energy_report(
+    def report_energy_estimate(
         self,
-        monitor_params,
-        monitor_hamiltonians,
-        monitor_energies,
-        E_errors,
+        monitor_dict,
+        iteration,
         writer=None,
-        logging_iter=None,
+        writer_iter=None,
+        num_samples=1e6,
+        max_unique=1e2,
     ):
+        """
+        Performs a complete survey of the status of ground state energy estimation
+        for the Hamiltonians in monitor_dict. This includes:
+        - Calculating the energy estimate (E_mean, E_var, Er, Ei)
+        - Updating the monitor_dict with the new energy estimate in its per-epoch buffer
+        - Logging the energy estimate to TensorBoard if a writer AND a writer_iter is provided
+
+        :param monitor_dict:
+            A dictionary containing the Hamiltonians to monitor, their parameters, and
+            the energy estimates for each epoch. See generate_monitor_dict in monitoring_dict.py
+            for a complete definition.
+        """
+
         with torch.no_grad():
-            for param in monitor_params:
-                for ham, E_ground in zip(monitor_hamiltonians, monitor_energies):
-                    E_mean, E_var, Er, Ei = self.extract_energy_estimate(ham, param)
-                    print("E_mean", E_mean, "E_ground", E_ground, "param", param)
-                    relative_error = torch.abs((E_mean - E_ground) / E_ground)
+            size_keys = monitor_dict.keys()
 
-                    E_errors.append(relative_error)
+            for size_key in size_keys:
+                param_keys = monitor_dict[size_key]["params"].keys()
 
-                    if (writer is not None) and (logging_iter is not None):
-                        writer.add_scalar(
-                            f"Energy_Error/N={ham.system_size.item()}, h={param.item()}",
-                            relative_error,
-                            logging_iter,
-                        )
-                    elif (writer is not None) or (logging_iter is not None):
-                        print(
-                            "Warning: writer and logging_iter must be both None or both not None. Setting both to None."
-                        )
-                        writer = None
-                        logging_iter = None
+                # Common across λJ.H(J)
+                size = monitor_dict[size_key]["system_size"]
+                H = monitor_dict[size_key]["H"]
 
-                    print(
-                        f"\tparam={param}, system_size={ham.system_size} - Relative Error: {relative_error}\n\t\tEnergy: {E_mean}, Variance: {E_var}, Real: {Er}, Imag: {Ei}"
+                for param_key in param_keys:
+
+                    # Common across H(J) for a particular J
+                    info_dict = monitor_dict[size_key]["params"][param_key]
+
+                    param = info_dict["param"]
+
+                    # Calculate the energy estimate
+                    E_mean, E_var, Er, Ei = self.extract_energy_estimate(
+                        H, param, num_samples, max_unique
                     )
+                    # TODO: why is E_mean imaginary?
+                    E_mean = torch.abs(E_mean)
+                    E_true = info_dict["energy"]
+                    error = E_mean - E_true
+
+                    # Update the monitor_dict with the new energy estimate
+                    info_dict["epoch_errors"].append(error)
+                    info_dict["epoch_relative_errors"].append(torch.abs(error) / E_true)
+                    info_dict["epoch_E_mean"].append(E_mean)
+                    info_dict["epoch_E_var"].append(E_var)
+                    info_dict["epoch_Er"].append(Er)
+                    info_dict["epoch_Ei"].append(Ei)
+
+                    # Log to TensorBoard
+                    if (writer is not None) and (writer_iter is not None):
+                        writer.add_scalar(
+                            f"Energy_Error/N={size.item()}, h={param.item()}",
+                            error,
+                            writer_iter,
+                        )
+                        writer.add_scalar(
+                            f"Energy/N={size.item()}, h={param.item()}",
+                            E_mean,
+                            writer_iter,
+                        )
+                        writer.add_scalar(
+                            f"Energy_Variance/N={size.item()}, h={param.item()}",
+                            E_var,
+                            writer_iter,
+                        )
+                        writer.add_scalar(
+                            f"Energy_Real/N={size.item()}, h={param.item()}",
+                            Er,
+                            writer_iter,
+                        )
+                        writer.add_scalar(
+                            f"Energy_Imag/N={size.item()}, h={param.item()}",
+                            Ei,
+                            writer_iter,
+                        )
 
     def extract_energy_estimate(
         self,
@@ -124,6 +190,7 @@ class Optimizer:
             max_unique: int
                 The maximum number of unique samples to generate (see sample in model_utils.py)
         """
+        # TODO: mark torch no grad
         self.model.set_param(system_size=H.system_size, param=param)
         symmetry = H.symmetry
         samples, sample_weight = sample(self.model, num_samples, max_unique, symmetry)
@@ -133,10 +200,10 @@ class Optimizer:
             (((E - E_mean).abs() ** 2 * sample_weight).sum() / H.n**2)
             .detach()
             .cpu()
-            .numpy()
+            # .numpy()
         )
-        Er = (E_mean.real / H.n).detach().cpu().numpy()
-        Ei = (E_mean.imag / H.n).detach().cpu().numpy()
+        Er = (E_mean.real / H.n).detach().cpu()  # .numpy()
+        Ei = (E_mean.imag / H.n).detach().cpu()  # .numpy()
 
         return E_mean, E_var, Er, Ei
 
@@ -226,20 +293,160 @@ class Optimizer:
 
         return log_prob, log_phase
 
+    def produce_save_str(self, ensemble_id=0):
+        name, embedding_size, n_head, n_layers = (
+            type(self.Hamiltonians[0]).__name__,
+            self.model.embedding_size,
+            self.model.n_head,
+            self.model.n_layers,
+        )
+
+        save_str = (
+            f"{name}_{embedding_size}_{n_head}_{n_layers}_{ensemble_id}_supervised"
+        )
+
+        return save_str
+
+    def consume_batch(
+        self,
+        H,
+        basis_states,
+        params,
+        psi_true,
+        writer,
+        writer_iter,
+        prob_weight,
+        arg_weight,
+        iter,
+    ):
+        self.model.set_param(system_size=H.system_size, param=None)
+
+        log_prob, log_phase = self.compute_psi_batch(
+            self.model,
+            params,
+            basis_states,
+            H.system_size,
+            symmetry=H.symmetry,
+            check_duplicates=True,
+        )
+
+        degenerate = params < 1
+
+        loss = prob_phase_loss(
+            log_prob,
+            log_phase,
+            psi_true,
+            degenerate=degenerate,
+            prob_weight=prob_weight,
+            arg_weight=arg_weight,
+            writer=writer,
+            writer_iter=writer_iter,
+        )
+
+        # TODO: how much of a slowdown does this cause?
+        unique_params = torch.unique(params)
+        param_max = torch.max(unique_params)
+        param_min = torch.min(unique_params)
+        print(f"\t\tIter {iter} - h ∈ [{param_min.item()}, {param_max.item()}]")
+
+        self.optim.zero_grad()
+        loss.backward()
+
+    def report_on_batch(
+        self,
+        monitor_dict,
+        global_iteration,
+        writer=None,
+    ):
+        # If there's a writer, plot gradient flow according to the
+        # self.grad_flow_iter_frequency
+        if (writer is not None) and (
+            global_iteration % self.grad_flow_iter_frequency == 0
+        ):
+            self.plot_grad_flow(
+                self.model.named_parameters(),
+                writer,
+                global_iteration,
+            )
+
+        # If a monitor_dict was passed, report energy estimates according
+        # to the self.energy_estimation_iter_frequency
+        if (monitor_dict is not None) and (
+            global_iteration % self.energy_estimation_iter_frequency == 0
+        ):
+            self.report_energy_estimate(
+                monitor_dict=monitor_dict,
+                iteration=global_iteration,
+                writer=writer,
+                writer_iter=global_iteration,
+            )
+
+    def report_on_epoch(
+        self,
+        monitor_dict,
+        global_iteration,
+        epoch,
+        writer=None,
+    ):
+        # Checkpoint the model every self.ckpt_freq epochs
+        if global_iteration % self.ckpt_freq == 0:
+            torch.save(
+                self.model.state_dict(),
+                f"{self.checkpointing_dir}/ckpt_iter{global_iteration}.ckpt",
+            )
+
+        # Average and clear the monitor_dict every epoch
+        # TODO: ensure this mutates the monitor_dict in place
+        average_monitor_dict(monitor_dict=monitor_dict, epoch=epoch)
+
+        # Display the averaged energy metrics in TensorBoard
+        if writer is not None:
+            for size_key in monitor_dict.keys():
+                for param_key in monitor_dict[size_key]["params"].keys():
+                    info_dict = monitor_dict[size_key]["params"][param_key]
+
+                    prefix = "Epoch_Average_Energy"
+
+                    # TODO: access element in info_dict corresponding to epoch
+                    # via a return value from average_monitor_dict instead of
+                    # indexing into info_dict
+
+                    writer.add_scalar(
+                        f"{prefix}_Error/N={size_key}, h={param_key}",
+                        info_dict["epoch_averaged_errors"][epoch],
+                        epoch,
+                    )
+                    writer.add_scalar(
+                        f"{prefix}/N={size_key}, h={param_key}",
+                        info_dict["epoch_averaged_E_mean"][epoch],
+                        epoch,
+                    )
+                    writer.add_scalar(
+                        f"{prefix}_Variance/N={size_key}, h={param_key}",
+                        info_dict["epoch_averaged_E_var"][epoch],
+                        epoch,
+                    )
+                    writer.add_scalar(
+                        f"{prefix}_Real/N={size_key}, h={param_key}",
+                        info_dict["epoch_averaged_Er"][epoch],
+                        epoch,
+                    )
+                    writer.add_scalar(
+                        f"{prefix}_Imag/N={size_key}, h={param_key}",
+                        info_dict["epoch_averaged_Ei"][epoch],
+                        epoch,
+                    )
+
     def train(
         self,
         epochs,
-        monitor_params=None,
-        monitor_hamiltonians=None,
-        monitor_energies=None,
+        monitor_dict=None,
         param_range=None,
+        log_tensorboard=False,
         ensemble_id=0,
         prob_weight=0.5,
         arg_weight=0.5,
-        writer: SummaryWriter = None,
-        run_num = 0,
-        start_epoch = 0,
-        energy_error_frequency = 100
+        start_epoch=0,
     ):
         """
         Trains the model to replicate the parameter-and-spin-sequence to ground
@@ -267,139 +474,71 @@ class Optimizer:
                 0.5 by default.
         """
 
-        timestep_plotting_index = 0
-
-        name, embedding_size, n_head, n_layers = (
-            type(self.Hamiltonians[0]).__name__,
-            self.model.embedding_size,
-            self.model.n_head,
-            self.model.n_layers,
+        self.checkpointing_dir, self.monitoring_dir, self.tensorboard_dir = (
+            create_save_dirs(self.produce_save_str(ensemble_id=ensemble_id))
         )
 
-        if param_range is None:
-            param_range = self.Hamiltonians[0].param_range
-        self.model.param_range = param_range
+        if log_tensorboard:
+            writer = SummaryWriter(self.tensorboard_dir)
+            print(
+                f"Use tensorboard --logdir {self.tensorboard_dir} for monitoring. Pass --bind-all if training remotely."
+            )
+        else:
+            writer = None
 
-        save_str = (
-            f"{name}_{embedding_size}_{n_head}_{n_layers}_{ensemble_id}_supervised"
-        )
+        total_iter = 0
 
-        self.E_errors_all = []
+        # if param_range is None:
+        #     param_range = self.Hamiltonians[0].param_range
+        # self.model.param_range = param_range
 
-        initial_energy = self.extract_energy_estimate(
-            monitor_hamiltonians[0], monitor_params[0]
-        )
-
-        initial_relative_error = torch.abs(
-            (initial_energy[0] - monitor_energies[0]) / monitor_energies[0]
-        )
-
-        print(f"Initial energy error: {initial_relative_error}")
-
-        for i in range(start_epoch, epochs):
-            epoch_start = time.time()
-
-            E_errors = []
-
-            iter = 0
+        for epoch in range(start_epoch, epochs):
+            print(f"Starting epoch {epoch}")
+            epoch_iter = 0
             random.shuffle(self.Hamiltonians)
             for H in self.Hamiltonians:
-                ham_start = time.time()
                 system_size = H.system_size
                 dataset = H.training_dataset
                 sampler = H.sampler
 
+                print(f"\tN = {system_size.tolist()}")
+
                 for sample_idx in sampler:
                     basis_states, params, psi_true = dataset[sample_idx]
 
-                    self.model.set_param(system_size=system_size, param=None)
-
-                    psi_start = time.time()
-
-                    log_prob, log_phase = self.compute_psi_batch(
-                        self.model,
-                        params,
-                        basis_states,
-                        system_size,
-                        symmetry=H.symmetry,
-                        check_duplicates=True,
-                    )
-
-                    psi_end = time.time()
-
-                    loss_time = time.time()
-
-                    degenerate = params < 1
-
-                    loss = prob_phase_loss(
-                        log_prob,
-                        log_phase,
-                        psi_true,
-                        degenerate=degenerate,
+                    self.consume_batch(
+                        H,
+                        basis_states=basis_states,
+                        params=params,
+                        psi_true=psi_true,
+                        writer=writer,
+                        writer_iter=total_iter,
                         prob_weight=prob_weight,
                         arg_weight=arg_weight,
+                        iter=epoch_iter,
+                    )
+
+                    self.report_on_batch(
+                        monitor_dict=monitor_dict,
+                        global_iteration=total_iter,
                         writer=writer,
-                        writer_iter=timestep_plotting_index,
                     )
 
-                    loss_end = time.time()
-
-                    unique_params = torch.unique(params)
-                    param_max = torch.max(unique_params)
-                    param_min = torch.min(unique_params)
-
-                    backprop_start = time.time()
-                    self.optim.zero_grad()
-                    loss.backward()
-                    if writer is not None:
-                        self.plot_grad_flow(
-                            self.model.named_parameters(),
-                            writer,
-                            timestep_plotting_index,
-                        )
+                    # TODO: can we move this to the consume_batch function
+                    # without causing issues with plot_grad_flow in report_on_batch?
                     self.optim.step()
-                    # scheduler.step()
-                    backprop_end = time.time()
 
-                    print(
-                        f"Epoch {i} iter {iter} - Loss for system size {system_size} and h-range {param_min}-{param_max}: {loss.item()}"
-                    )
+                    epoch_iter += 1
+                    total_iter += 1
 
-                    if timestep_plotting_index % self.ckpt_freq == 0:
-                        if not os.path.isdir("supervised_results"):
-                            os.mkdir("supervised_results")
-                        torch.save(
-                            self.model.state_dict(),
-                            f"supervised_results/ckpt_run{run_num}_{timestep_plotting_index}_{save_str}.ckpt",
-                        )
+            self.report_on_epoch(
+                monitor_dict=monitor_dict,
+                global_iteration=total_iter,
+                epoch=epoch,
+                writer=writer,
+            )
 
-                    energy_start = time.time()
-
-                    if (not monitor_params is None) and (timestep_plotting_index % energy_error_frequency == 0):
-                        self.show_energy_report(
-                            monitor_params,
-                            monitor_hamiltonians,
-                            monitor_energies,
-                            E_errors,
-                            writer,
-                            logging_iter=timestep_plotting_index,
-                        )
-
-                    energy_end = time.time()
-
-                    print(
-                        f"Time breakdown: psi: {psi_end - psi_start}, loss: {loss_end - loss_time}, backprop: {backprop_end - backprop_start}, energy retrieval: {energy_end - energy_start}"
-                    )
-
-                    iter += 1
-                    timestep_plotting_index += 1
-
-                ham_end = time.time()
-                print(
-                    f"Hamiltonian for size {system_size} took {ham_end - ham_start} seconds"
-                )
-
-            self.E_errors_all.append(E_errors)
-
-            epoch_end = time.time()
-            print(f"Epoch {i} took {epoch_end - epoch_start} seconds")
+        pickle.dump(
+            monitor_dict,
+            open(f"{self.monitoring_dir}/monitor_dict.pkl", "wb"),
+        )
